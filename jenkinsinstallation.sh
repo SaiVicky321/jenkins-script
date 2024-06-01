8c2a37e9011de1daaeb9c57702c0b585a78ef66b
SaiVicky321 <163331916+SaiVicky321@users.noreply.github.com>
2024-06-01 23:30:00 +0530

    Updated jenkinsinstallation.sh

--- jenkinsinstallation.sh
+++ jenkinsinstallation.sh
@@ -1,31 +1,13 @@
 #!/bin/bash
 sudo apt update -y
-
-#Installation of JAVA
 sudo apt install openjdk-11-jdk -y
-
-#Installation of Jenkins
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-
+sudo apt update -y
+sudo apt install openjdk-8-jdk -y
+sudo apt install maven -y
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-
-sudo apt-get update
-
+sudo apt-get update -y
 sudo apt-get install jenkins -y
-
-#Enable Jenkins:
-sudo systemctl enable jenkins
-
-#Start Jenkins:
-sudo systemctl start jenkins
-
-if [STATUS == 'active']
-   then
-   echo "Jenkins is running"
-else
-   echo "Jenkins is not running, starting the service"
-   systemctl start jenkins
-fi
-
