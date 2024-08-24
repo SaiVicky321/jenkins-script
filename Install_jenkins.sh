52c2ac5978abc377e7d9c06b261ca1811ac27f04
SaiVicky321 <163331916+SaiVicky321@users.noreply.github.com>
2024-08-24 11:00:20 +0530

    Add installation files for jenkins and docker

---
+++ Install_jenkins.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+sudo apt update
+sudo apt install openjdk-11-jdk -y
+
+# Add Jenkins repository key
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+# Add Jenkins repository to the system
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+# Update package list and install Jenkins
+sudo apt update
+sudo apt install jenkins -y
+
+# Start and enable Jenkins
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
